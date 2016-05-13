4bb1e641957758b3c2f370305f2068a1014c7693
AndriyUkrajnets <aukrajnets@gmail.com>
2016-05-13 17:27:01 +0200

    My blog start work

---
+++ javascripts/script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " andrij-y. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Головна</a></li>" +
+"<li><a href='customers.html'>Дані</a></li>" +
+"<li><a href='about.html'>Про нас</a></li>" +
+"</ul>";
