fb8480ccd1b0e9faaa1fd6e1b37d78f976a180e2
AndriyUkrajnets <aukrajnets@gmail.com>
2016-05-27 13:25:28 +0200

    new sait templite

--- javascripts/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " andrij-y. All rights reserved.</p>";
-
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Головна</a></li>" +
-"<li><a href='customers.html'>Дані</a></li>" +
-"<li><a href='about.html'>Про нас</a></li>" +
-"</ul>";
